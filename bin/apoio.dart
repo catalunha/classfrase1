7a320ccb9cf3f01cae6a835417c69e4c8a903f7b
catalunha <catalunha.mj@gmail.com>
2021-10-07 16:00:29 -0300

    revisao

---
+++ bin/apoio.dart
@@ -0,0 +1,50 @@
+  // String phrase =
+  //     'Palavra a b c Ponto. Ponto . Virgula, Ponto e vírgula; Dois pontos: Reticencias... ou ...Reticentias ou ... Reticentias Travessão x-y Parenteses (m) ou ( n ) "Aspas" ou " Aspas " Exclamação! Interrogação ? R\$40.00';
+
+
+  // print(phrase.replaceAll('-', ' - ').split(' '));
+  // List<String> phraseList = phrase.replaceAll('-', ' - ').split(' ');
+  // print(
+  //     phrase.split(' ').join('|').replaceAll('.', '|.').replaceAll('||', '|'));
+
+  // for (var item in phrase.) {}
+  // var phraseList = phrase
+  //     .replaceAll('...', ' ... ')
+  //     // .replaceAll('.', ' . ')
+  //     .replaceAll(',', ' , ')
+  //     .replaceAll(';', ' ; ')
+  //     .replaceAll('!', ' ! ')
+  //     .replaceAll('?', ' ? ')
+  //     .replaceAll('--', ' -- ')
+  //     .replaceAll('-', ' - ')
+  //     .replaceAll('"', ' " ')
+  //     .replaceAll(':', ' : ')
+  //     .replaceAll('(', ' ( ')
+  //     .replaceAll(')', ' ) ')
+  //     .replaceAll('  ', ' ')
+  //     .replaceAll('  ', ' ')
+  //     .split(' ');
+  // print(phraseList.join('|'));
+  // print(phraseList.join('|').replaceAll('.', '|.'));
+  // print(phraseList.join('|').replaceAll('.', '|.').split('|'));
+  // // phraseList = phraseList.join('|').replaceAll('.', '|.|').split('|');
+  // print(phraseList);
+  // var phraseList2 = phrase
+  //     .replaceAll('.', '')
+  //     .replaceAll(',', '')
+  //     .replaceAll(';', '')
+  //     .replaceAll('!', '')
+  //     .replaceAll('?', '')
+  //     .replaceAll('--', '')
+  //     .replaceAll('-', ' ')
+  //     .replaceAll('"', '')
+  //     .replaceAll(':', '')
+  //     .replaceAll('...', '')
+  //     .replaceAll('(', '')
+  //     .replaceAll(')', '')
+  //     .replaceAll('  ', ' ')
+  //     .replaceAll('   ', ' ')
+  //     .split(' ');
+  // print(phraseList2);
+  // var phraseList3 = phrase.replaceAll('-', ' ').split(' ');
+  // print(phraseList3);
